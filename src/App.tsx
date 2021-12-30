4fc7e4d6f505e356339130275b237c4b0459bdaf
cain <chen562450669@qq.com>
2021-12-30 14:21:50 +0800

    refactor: btn -> cover antd style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ButtonPage from './pages/ButtonPage'
 // import InputPage from './pages/InputPage'
+import AntdPgae from './pages/antd/AntdPage'
 
 function App() {
   return (
     <div className="App">
+      <AntdPgae />
       <ButtonPage />
       {/* <InputPage /> */}
     </div>
---
+++ src/pages/antd/AntdPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Button} from 'antd'
+import './index.css'
+
+const AntdPgae = () => {
+  return <Button>antd btn</Button>
+}
+
+export default AntdPgae
