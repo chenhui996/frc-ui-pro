5c1a9cb52e927da9de89dd22e4e50fa1021e79a8
陈辉 <chenhui@chenhuideMac-mini.local>
2022-01-18 17:35:24 +0800

    feat: init public

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ButtonPage from './pages/ButtonPage'
+// import ButtonPage from './pages/ButtonPage'
 // import InputPage from './pages/InputPage'
 // import InputNumber from './pages/InputNumberPage'
 // import SelectPage from './pages/SelectPage'
@@ -13,7 +13,7 @@ import ButtonPage from './pages/ButtonPage'
 function App() {
   return (
     <div className="App" style={{ paddingBottom: 500 }}>
-      <ButtonPage />
+      {/* <ButtonPage /> */}
       {/* <InputPage /> */}
       {/* <InputNumber /> */}
       {/* <SelectPage /> */}
